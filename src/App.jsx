213c5369a73af18ed08708df400a4a8c899f1d4c
Austen Turner <austenpturner@msn.com>
2024-07-10 14:16:34 -0700

    set up main, import summary & expense view

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Container, Flex, Box } from "@chakra-ui/react";
+import Main from "./components/main";
+
 function App() {
-  return <></>;
+  return (
+    <Container bg={"#f8fafd"} maxW={"Container.3xl"} height={"100vh"} p={"0"}>
+      <Flex height={"full"}>
+        <Box height={"full"} flex={5} w={["20%", "30%", "20%", "50%", "60%"]}>
+          <Main />
+        </Box>
+      </Flex>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/expense-view/index.jsx
@@ -0,0 +1,3 @@
+export default function ExpenseView() {
+  return <div>expense view</div>;
+}
